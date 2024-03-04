c0369c0cd57751625be4be843e3d9077b1f4df35
Lisa Chen <lisabear@Lisas-MBP.lan>
2024-03-04 11:51:39 -0800

    class files

---
+++ rscript of graph.R
@@ -0,0 +1,15 @@
+"class 04- lisa chen- 1-18-24"
+#My First R script
+ <-1:50
+plot(x,sin(x))
+plot(x,sin(x), typ="l")
+plot(x,sin(x), typ="l", col="blue")
+plot(x,sin(x), typ="l", col="blue", lwd=2)
+plot(x,sin(x), typ="l", col="blue", lwd=2, xlab="Hello")
+
+plot(x,sin(x), typ="l", col="red", lwd=2, xlab)
+
+plot(x,sin(x), typ="l", col="red", lwd=2, xlab="Hello")
+
+
+
